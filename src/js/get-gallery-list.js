ddc7660c81acea313b3f1376198bc98964ccd1ea
Iryna <irinkasatanovskaya@gmail.com>
2023-05-12 19:17:17 +0300

    add styles

--- src/js/get-gallery-list.js
+++ src/js/get-gallery-list.js
@@ -1,8 +1,8 @@
 export const galleryItem = galleryItems.map(item => {
   return `<li class="gallery-item">
    <a class="gallery-link" href="">
-      <img class="gallery__image" src="${c.thumbnail.path}/${imageSize}.${c.thumbnail.extension}"" alt="" />
+      <img class="gallery-image" src="${c.thumbnail.path}/${imageSize}.${c.thumbnail.extension}"" alt="" />
+      <p class="hero-name">${item.name}</p>
    </a>
-   <p class="hero-name"></p>
 </li>`;
 });
