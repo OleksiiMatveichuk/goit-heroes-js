f63f455553cea8bde80fddcba8342861a667110d
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-05-14 02:10:45 +0300

    promise

--- src/js/random-list.js
+++ src/js/random-list.js
@@ -1,42 +1,36 @@
+//import { resolveBaseUrl } from 'vite';
 import { createModal1 } from './createModal1';
+import { api } from './low-level/api';
 
-const randomList = [
-  {
-    id: 1,
-    name: 'Ant-Man',
-    text: 'Thief turned hero Scott Lang uses size manipulation tech to infiltrate bases and intimidate bad guys.',
-  },
-  {
-    id: 2,
-    name: 'The Wasp ',
-    text: 'Hope Van Dyne, daughter of the brilliant and heroic Ant-Man (Hank Pym) and The Wasp (Janet van Dyne), helps trains her father’s successor Scott Lang and secure the destruction of the Yellowjacket technology.',
-  },
-  {
-    id: 3,
-    name: 'Cassie Lang',
-    text: 'As Scott Lang’s young daughter, Cassie is super proud of her dad’s heroics as Ant-Man.',
-  },
-  {
-    id: 4,
-    name: 'Kang The Conqueror',
-    text: 'The character is most frequently depicted as an opponent of the Avengers and the Fantastic Four.',
-  },
-  {
-    id: 5,
-    name: 'Hank Pym',
-    text: 'Hank Pym is a brilliant scientist who used his discovery of the shrinking Pym Particles, the suit that could survive the process, and insect communication waves, to turn himself into the original Ant-Man.',
-  },
-];
-
+const listCharacters = [];
 const ulList = document.querySelector('.random-list');
 const clickBtn = document.querySelector('.allCharactersBtn');
 clickBtn.addEventListener('click', createModal1);
+const img = document.querySelector('.random-img');
 
 ulList.addEventListener('click', handleClickItem);
+
+async function getRandomData(params) {
+  const data = await api.getAllCharacters(params);
+
+  listCharacters.push(data.results);
+}
+
+function newImg({ resourceURI }) {
+  img.src = `${resourceURI}`;
+}
+
 function handleClickItem(e) {
   if (e.target.tagName !== 'UL') {
     let li = document.querySelectorAll('li');
     const id = e.target.closest('li').id;
+
+    const result = listCharacters.find(
+      character => character[0].id === Number(id)
+    );
+
+    newImg(result[0]);
+
     let i = 0;
     li.forEach(item => {
       let activeLink = document.querySelector('.random-item.active');
@@ -62,15 +56,49 @@ function handleClickItem(e) {
 
 function createLi(value) {
   return `
-    <li class='random-item' id=${value.id}>
-       <h3 class='random-value-name hero-name'>${value.name}</h3>
-       <p class='random-value-text'>${value.text}</p>
+    <li class='random-item' id=${value[0].id}>
+       <h3 class='random-value-name hero-name'>${value[0].name}</h3>
+       <p class='random-value-text'>${value[0].description}</p>
     </li>
     `;
 }
 
 function gerRandomCharacters(array) {
+  console.log(array);
   return array.map(character => createLi(character)).join('');
 }
-const markup = gerRandomCharacters(randomList);
-ulList.innerHTML = markup;
+
+async function startMain() {
+  // for (let i = 0; i < 5; i += 1) {
+  //   await getRandomData({
+  //     limit: 1,
+  //     offset: Math.round(Math.random() * 1561),
+  //   });
+  // }
+  const d1 = new Date();
+  await promise();
+  const d2 = new Date();
+  console.log('time: ', (d2 - d1) / 60);
+  const markup = gerRandomCharacters(listCharacters);
+  ulList.innerHTML = markup;
+  const newIMG = document.createElement('img');
+  newIMG.src = listCharacters[0][0].resourceURI;
+  ulList.insertAdjacentElement('beforebegin', newIMG);
+  img.src = listCharacters[0][0].resourceURI;
+}
+startMain();
+
+//================promise
+
+async function promise() {
+  const promiseArray = [];
+  for (let i = 0; i < 5; i += 1) {
+    const res = await getRandomData({
+      limit: 1,
+      offset: Math.round(Math.random() * 1561),
+    });
+    // promiseArray.push(res);
+  }
+  // console.log(promiseArray);
+  Promise.all(promiseArray);
+}
