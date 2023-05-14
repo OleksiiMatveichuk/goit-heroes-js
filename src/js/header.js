f03f771e01747bdf019416867d7ed3d257e51d36
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-05-14 19:05:24 +0300

    search with api and gallery

--- src/js/header.js
+++ src/js/header.js
@@ -11,8 +11,6 @@ searchInput.addEventListener('click', () => {
 });
 
 searchInput.addEventListener('input', debounce(inputHandler, 500));
-
-
 async function inputHandler() {
     const query = this.value.trim();
 
@@ -42,16 +40,77 @@ async function inputHandler() {
     this.setAttribute('list', 'search-results');
 }
 
+// ШУКАЮ ШИРИНУ КОНТЕЙНЕРА
+const container = document.querySelector(".container")
+let windowWidth = window.getComputedStyle(container).width;
+let itemsOnPage = null;
+// ВИЗНАЧАЄМО ШИРИНУ ВЬЮПОРТУ
+// debugger
+switch (windowWidth) {
+    case '375px':
+        itemsOnPage = 5;
+        break;
+    case '100%':
+        itemsOnPage = 5;
+        break;
+    case '1440px':
+        itemsOnPage = 16;
+        break;
+
+    default:
+        itemsOnPage = 8;
+        break;
+}
+
+window.addEventListener('load', async () => {
+    if (window.location.pathname.includes('page-2.html')) {
+        const savedValue = localStorage.getItem('searchValue');
+        if (savedValue) {
+            // Если значение сохранено, вставляем его в инпут
+            const searchInput = document.querySelector('.header-input');
+            searchInput.value = savedValue;
+        }
+        const galleryList = document.querySelector('.gallery');
+        galleryList.innerHTML = '';
+        galleryList.innerHTML = await createGallery(savedValue || '');
+    }
+});
 
 // Go to new window
+
 const form = document.querySelector('#search-form');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    // Получаем значение из поля ввода
-    const searchQuery = searchInput.value.trim();
-    // Формируем URL
-    const url = `../page-2.html?searchQuery=${searchQuery}`;
-    window.location.href = url;
-
+    let searchQuery = searchInput.value.trim();
+    const url = `./page-2.html`;
+    if ((window.location.href === window.location.origin + "/") || window.location.pathname.includes("index.html")) {
+        localStorage.setItem('searchValue', searchQuery);
+        window.location.href = url;
+    } else if (window.location.pathname.includes("page-2.html")) {
+        searchQuery = searchInput.value
+        const galleryList = document.querySelector('.gallery');
+        galleryList.innerHTML = ''
+        console.log("input", searchQuery)
+        galleryList.innerHTML = await createGallery(searchQuery)
+        localStorage.removeItem('searchValue');
+    }
 });
+
+import { galleryItem, renderGallery } from "./get-gallery-list"
+async function createGallery(searchQuery) {
+    try {
+        console.log("itemsOnPage=", itemsOnPage)
+        const data = await api.getAllCharacters({ nameStartsWith: searchQuery, limit: itemsOnPage, });
+        const results = data.results
+        console.log(data)
+        console.log(results)
+        if (results.length === 0) {
+            console.log("NOT FOUND!!!!")
+            return
+        }
+        return renderGallery(results)
+    } catch (e) { console.log(e) }
+
+}
+
