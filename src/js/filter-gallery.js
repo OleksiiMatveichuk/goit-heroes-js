b6de139167555ca1ab7527627c7b914f79c6b919
Iryna <irinkasatanovskaya@gmail.com>
2023-05-16 10:46:03 +0300

    update

--- src/js/filter-gallery.js
+++ src/js/filter-gallery.js
@@ -6,43 +6,48 @@ const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.form');
 const comicsFilter = document.querySelector('#comic');
 
-form.addEventListener('input', debounce(handleInput, 300));
+const container = document.querySelector('.container');
+let windowWidth = window.getComputedStyle(container).width;
+let itemsOnPage = null;
+// ВИЗНАЧАЄМО ШИРИНУ ВЬЮПОРТУ
+// debugger
+switch (windowWidth) {
+  case '375px':
+    itemsOnPage = 5;
+    break;
+  case '100%':
+    itemsOnPage = 5;
+    break;
+  case '1440px':
+    itemsOnPage = 16;
+    break;
+
+  default:
+    itemsOnPage = 8;
+    break;
+}
+
+comicsFilter.addEventListener('input', debounce(handleInput, 300));
 
 async function handleInput(event) {
   event.preventDefault();
   createGallery();
+  gallery.innerHTML = '';
+  gallery.innerHTML = await createGallery();
 }
 
 async function createGallery() {
   try {
     const searchQuery = comicsFilter.value.trim();
-    const data = await api.getCharactersByComicsId({ comicId: searchQuery });
+
+    const data = await api.getAllCharacters({
+      comics: searchQuery,
+      limit: itemsOnPage,
+    });
     const result = data.results;
 
-    const heroCard = renderGallery(data);
-    gallery.insertAdjacentHTML('beforeend', heroCard.join(''));
+    const heroCard = renderGallery(result);
   } catch {
     console.log('error');
   }
 }
-
-// const container = document.querySelector('.container');
-// let windowWidth = window.getComputedStyle(container).width;
-// let itemsOnPage = null;
-// // ВИЗНАЧАЄМО ШИРИНУ ВЬЮПОРТУ
-// // debugger
-// switch (windowWidth) {
-//   case '375px':
-//     itemsOnPage = 5;
-//     break;
-//   case '100%':
-//     itemsOnPage = 5;
-//     break;
-//   case '1440px':
-//     itemsOnPage = 16;
-//     break;
-
-//   default:
-//     itemsOnPage = 8;
-//     break;
-// }
