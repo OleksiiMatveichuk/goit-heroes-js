7ea6959eac6949fcf8b159b26cd2866de82e98ca
Tensys57 <tensys57@gmail.com>
2023-05-17 11:51:59 +0300

    Fixed logo size at the footer

--- src/js/low-level/keys.js
+++ src/js/low-level/keys.js
@@ -1,13 +1,12 @@
 // keys for api
 
-<<<<<<< Updated upstream
-=======
-const keys = [
-  {
-    publickey: '6098703cb93535806af01869e44e21c4',
-    privatekey: 'fc32c1d6f8408c5efa07ea334cecc41e18ba0403',
-  },
-];
+// <<<<<<< Updated upstream
+// const keys = [
+//   {
+//     publickey: '6098703cb93535806af01869e44e21c4',
+//     privatekey: 'fc32c1d6f8408c5efa07ea334cecc41e18ba0403',
+//   },
+// ];
 //   {
 //     publickey: '3e8547eb41b42a9a2a1ebdd17f3eee37',
 //     privatekey: '1eb0a59387ba717430ceb4d1fa44d25acc6fdeb0',
@@ -18,7 +17,7 @@ const keys = [
 //   },
 // ];
 // export const key = keys[Math.round(Math.random() * (keys.length - 1))];
->>>>>>> Stashed changes
+// >>>>>>> Stashed changes
 // export const key = {
 //   publickey: '6098703cb93535806af01869e44e21c4',
 //   privatekey: 'fc32c1d6f8408c5efa07ea334cecc41e18ba0403',
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,55 +1,41 @@
-import '../css/pagination.css';
-import { createFilterGallery } from './all_characters_filter';
+// import '../css/pagination.css';
+// import { createFilterGallery } from './all_characters_filter';
 
-const galleryList = document.querySelector('.gallery');
-console.log('galleryList :>> ', galleryList.attributes);
-
-const limits = galleryList.dataset.limits;
-const offset = galleryList.dataset.offset;
-const total = galleryList.dataset.total;
-console.log('limits:>> ', limits);
-console.log('offset :>> ', offset);
-console.log('total :>> ', total);
-console.log('galleryList.dataset :>> ', galleryList.dataset);
-console.log(galleryList.getDataAttr);
-const params = Array.from(galleryList.dataset);
-console.log('galleryList.length :>> ', galleryList.length);
-
-const refs = {
-  sectionFilter: document.querySelector('.section-filter'),
-};
+// const galleryList = document.querySelector('.gallery');
+// const limits = galleryList.dataset.limits;
+// const offset = galleryList.dataset.offset;
+// const total = galleryList.dataset.total;
+// console.log('galleryList :>> ', galleryList.attributes);
+// console.log('galleryList.dataset :>> ', galleryList.dataset);
+// console.log('limits:>> ', limits);
+// console.log('offset :>> ', offset);
+// console.log('total :>> ', total);
 
 // refs.paginationList.addEventListener('click', requestHandler);
 
-export function createPaginationList(total, limit, offset) {
-  // paginationList.innerHTML = '';
-  if (total < 1) {
-    return;
-  }
-  const arr = [];
-  const pages = Math.ceil(total / limit);
-  const activePage = Math.ceil(offset / limit) + 1;
-  for (let i = activePage; i <= pages; i++) {
-    const li = document.createElement('li');
-    li.textContent = i;
-    arr.push(li);
-  }
-  galleryList.append(...arr);
-  console.log('pages :>> ', pages);
-  console.log('activePage :>> ', activePage);
-}
+// export function createPaginationList(total, limit, offset) {
+//   // paginationList.innerHTML = '';
+//   if (total < 1) {
+//     return;
+//   }
+//   const arr = [];
+//   const pages = Math.ceil(total / limit);
+//   const activePage = Math.ceil(offset / limit) + 1;
+//   for (let i = activePage; i <= pages; i++) {
+//     const li = document.createElement('li');
+//     li.textContent = i;
+//     arr.push(li);
+//   }
+//   galleryList.append(...arr);
 
-createPaginationList(220, 8, 40);
-function handlePagination(target) {
-  if ((target.localName = 'li')) {
-    const offset = target.textContent * limit;
-    console.log('offset :>> ', offset);
-    getAllCharacters(offset);
-    return offset;
-  }
-}
+// }
 
-// export const key = {
-//   publickey: '3e8547eb41b42a9a2a1ebdd17f3eee37',
-//   privatekey: '1eb0a59387ba717430ceb4d1fa44d25acc6fdeb0',
-// };
+// createPaginationList(220, 8, 40);
+// function handlePagination(target) {
+//   if ((target.localName = 'li')) {
+//     const offset = target.textContent * limit;
+//     console.log('offset :>> ', offset);
+//     getAllCharacters(offset);
+//     return offset;
+//   }
+// }
